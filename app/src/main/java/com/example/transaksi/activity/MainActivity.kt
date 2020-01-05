22dd3df72bf6d5ada3a3bf2859e1e04c3d4342b4
rizkiar10154 <rizkyarif196@gmail.com>
2020-01-06 02:31:51 +0700

    Initial commit

---
+++ app/src/main/java/com/example/transaksi/activity/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.example.transaksi.activity
+
+import android.content.Context
+import android.content.pm.PackageInstaller
+import android.media.session.MediaSessionManager
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.transaksi.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+
+        Context
+
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+}
